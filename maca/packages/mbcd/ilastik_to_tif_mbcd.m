096b6ef19f5634bf4ac5d65616810f1df1ebbb34
Will Gray Roncal <wgr@jhu.edu>
2016-01-28 15:18:25 -0500

    adding licensing info

--- maca/packages/mbcd/ilastik_to_tif_mbcd.m
+++ maca/packages/mbcd/ilastik_to_tif_mbcd.m
@@ -12,3 +12,18 @@ prob = im2uint16(prob); %matlab stores these files best as uint16
 
 %figure, imagesc(prob)
 imwrite(prob, fileOut)
+
+% (c) 2015 The Johns Hopkins University / Applied Physics Laboratory 
+% All Rights Reserved. Contact the JHU/APL Office of Technology Transfer for any # additional rights.  www.jhuapl.edu/ott
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%    http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
